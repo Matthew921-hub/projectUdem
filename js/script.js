28044984038592371b3aa48ff94a8f2c2dc39584
Matthew <vaskovitch.matvej@yandex.ru>
2021-08-23 18:13:25 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//alert('hello world');
+
+//confirm('Age:');
+
+let answers = [];
+answers[0] = prompt("What is your name?", "");
+answers[1] = prompt("How old are you?", "");
+answers[2] = prompt("Where are you from?", "");
+
+document.write(answers[0], answers[1], answers[2]);
